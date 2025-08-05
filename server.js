4608fc185ae1d1db7de889d13fca41de94dcd2a5
Ankit kumar <ak8068716@gmail.com>
2025-08-05 13:34:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const db = require('./config/db');
+db();
+
+app.use(express.json());
+app.use(cors());
+app.use('/api/auth', authRoutes);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+
+
+
+
+
+
+
+
